456feffa5c8f89c5849bc134f49d1c9c261cbdf7
fs440920 <fs440920@gmail.com>
2025-01-14 14:01:09 +0500

    Updates

---
+++ precache-manifest.e89c9f8fe1e44cf041d60ff922aac013.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a46737c6c2a918c3ca5d631dd1ea818",
+    "url": "/textutils/index.html"
+  },
+  {
+    "revision": "68a1b6debd384635110e",
+    "url": "/textutils/static/css/main.68afde09.chunk.css"
+  },
+  {
+    "revision": "c9f22a7475de69570966",
+    "url": "/textutils/static/js/2.60114fd2.chunk.js"
+  },
+  {
+    "revision": "65b09d4299c06eb6027e319e95c4f0dc",
+    "url": "/textutils/static/js/2.60114fd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae626bc705c0bd239f8a",
+    "url": "/textutils/static/js/3.7ba09831.chunk.js"
+  },
+  {
+    "revision": "68a1b6debd384635110e",
+    "url": "/textutils/static/js/main.4883f5fd.chunk.js"
+  },
+  {
+    "revision": "09ed687c7c3963878f26",
+    "url": "/textutils/static/js/runtime-main.5c304625.js"
+  }
+]);
