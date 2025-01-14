376e5aeceedb43d35e8d47abd1fca1d4e9e41894
fs440920 <fs440920@gmail.com>
2025-01-14 13:55:23 +0500

    Add GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -4,76 +4,75 @@ import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
 import React, { useState } from "react";
 import Alert from "./components/Alert";
-// import {
-//   BrowserRouter as Router,
-//   Routes, // Replaces Switch
-//   Route,
-//   Link
-// } from "react-router-dom";
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
   
-  const showAlert = (message, type)=> {
+  const showAlert = (message, type) => {
     setAlert({
-      msg:message,
-      type:type
-    })
+      msg: message,
+      type: type,
+    });
     setTimeout(() => {
       setAlert(null);
     }, 1500);
-  }
+  };
 
-const toggleMode = () => {
-  if (mode === "light") {
-    setMode("dark");
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
       document.body.style.backgroundColor = "rgb(53, 53, 53)";
       document.body.style.color = "white";
-      showAlert("Dark mode has been enabled", ("success"))
-  }  
-  else {
-    setMode("light");
-    document.body.style.backgroundColor = "white";
-    document.body.style.color = "black";
-    showAlert("Light mode has been enabled", ("success"))
-  }
-};
+      showAlert("Dark mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      document.body.style.color = "black";
+      showAlert("Light mode has been enabled", "success");
+    }
+  };
 
-return (
-  <>
-    {/* <Router> */}
-      {/* Navbar */}
-      <Navbar
-        title="TextUtils"
-        aboutText="About Us"
-        mode={mode}
-        toggleMode={toggleMode}
-      />
-      {/* Navbar end */}
-
-      {/* Alert */}
-      <Alert alert={alert} />
-      {/* Alert end */}
-
-      {/* Container */}
-      <div className="container my-3">
-        {/* <Routes> */}
-          {/* Replaces Switch */}
-          {/* <Route exact path="/about" element={<About />} /> */}
-
-          {/* <Route exact path="/"element= */}
-          {<Textform showAlert={showAlert}heading="Enter the text to analyze below"mode={mode}/>}
-          {/* /> */}
-        {/* </Routes> */}
-      </div>
-      {/* Container end */}
-    {/* </Router> */}
-    
+  return (
+    <>
+      <Router basename="/textutils">
+        {/* Navbar */}
+        <Navbar
+          title="TextUtils"
+          aboutText="About Us"
+          mode={mode}
+          toggleMode={toggleMode}
+        />
+        {/* Alert */}
+        <Alert alert={alert} />
+        {/* Container */}
+        <div className="container my-3">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <Textform
+                  showAlert={showAlert}
+                  heading="Enter the text to analyze below"
+                  mode={mode}
+                />
+              }
+            />
+            {/* Uncomment this if "About" page is added */}
+            {/* <Route exact path="/about" element={<About />} /> */}
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
 
 export default App;
+
 
